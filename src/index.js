6beb7e865b0371a1b0ca42380d424be5fc6cb26a
Jane Costa <yberdysh@gmail.com>
2019-02-01 11:02:18 -0500

    filter functionality working

--- src/index.js
+++ src/index.js
@@ -2,14 +2,18 @@ document.addEventListener('DOMContentLoaded', () => {
   // console.log(POKEMON)
   //YOUR CODE HERE
   let pokeContainer = document.querySelector("#pokemon-container")
-  const input = document.querySelector("#pokemon-search-form input");
-  console.log("input", input);
-  fetchPokes()
-  input.addEventListener("keyUp", filterPokes)
+  const input = document.querySelector("#pokemon-search-form");
+  initialLoadPokes()
+  // input.addEventListener("input", filterPokes)
+  input.addEventListener("input", filterPokes)
 
   function fetchPokes(){
-     fetch("http://localhost:3000/pokemon")
+     return fetch("http://localhost:3000/pokemon")
     .then(res => res.json())
+  }
+
+  function initialLoadPokes(){
+    fetchPokes()
     .then(putPokesOnDOM)
   }
 
@@ -24,21 +28,29 @@ document.addEventListener('DOMContentLoaded', () => {
         </div>
       </div>
     </div>`
+
     })
   }
 
   function filterPokes(event){
-    console.log("hello world")
+    let string = event.target.value
+    pokeContainer.innerHTML = ""
+    fetchPokes()
+    // .then(console.log)
+    .then(pokemons => pokemons.filter(pokemon => pokemon.name.includes(string)))
+    .then(putPokesOnDOM)
+
     // console.log(event)
 
   }
 
-  // function flip(pokemonId){
-  //   fetch(`http://localhost:3000/pokemon/${pokemon.id}`)
-  //   .then(res => res.json())
-  //   .then(pokemon => {
-  //     document.querySelector(`.pokemon-image img[data-id=${pokemon.id}]`)
-  //   })
-  // }
+  function flip(pokemonId){
+    console.log("flipping...")
+    // fetch(`http://localhost:3000/pokemon/${pokemon.id}`)
+    // .then(res => res.json())
+    // .then(pokemon => {
+    //   document.querySelector(`.pokemon-image img[data-id=${pokemon.id}]`)
+    // })
+  }
 
 })
